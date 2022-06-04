109ec55cb23d503cfea2964fcf87f17a493ca4aa
YuraS <yurastepanenko@gmail.com>
2022-06-05 00:09:34 +0300

    change view posts in admins panel

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+# Отвечает за отображение в админке сайта
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+
+
+# Подключаем к админке сайта
+admin.site.register(Post, PostAdmin)
