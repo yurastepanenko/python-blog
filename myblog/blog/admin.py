379c233bd7e579d3011fda5dcc64101230a34009
YuraS <yurastepanenko@gmail.com>
2022-06-05 00:32:32 +0300

    finished modify admin.py

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models import Post
 
+
 # Отвечает за отображение в админке сайта
 class PostAdmin(admin.ModelAdmin):
+    # Поля, которые будут отображаться
     list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    # raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
 
 
 # Подключаем к админке сайта
