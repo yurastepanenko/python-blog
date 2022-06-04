34b2d4e87bb134b580839bd7502e71237869eb8f
YuraS <yurastepanenko@gmail.com>
2022-06-05 01:00:42 +0300

    add urls

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -17,6 +18,13 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES,
                               default='draft')
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug])
 
 
 class Meta:
---
+++ myblog/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    # post views
+    path(r'', views.post_list, name='post_list'),
+    re_path(r'(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/' \
+            r'(?P<post>[-\w]+)/',
+            views.post_detail,
+            name='post_detail'),
+]
