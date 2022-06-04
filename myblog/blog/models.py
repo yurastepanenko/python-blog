be52fe37655c70cfc90df4e40594e61efaeececd
YuraS <yurastepanenko@gmail.com>
2022-06-04 23:57:47 +0300

    add user yura + add Post in admins panel

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -9,15 +9,19 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='blog_posts',
+                               on_delete=models.CASCADE)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES,
+                              default='draft')
+
 
 class Meta:
     ordering = ('-publish',)
+
 
 def __str__(self):
     return self.title
