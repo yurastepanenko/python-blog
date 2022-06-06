ccc1354e3141c7bf86e9a98dabb837e28623cae5
YuraS <yurastepanenko@gmail.com>
2022-06-06 21:58:04 +0300

    add paginator

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Список опубликованных статей
+# def post_list(request):
+#     # return render(request, 'blog/post/list.html')
+#     posts = Post.objects.filter(status="published")
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 def post_list(request):
-    # return render(request, 'blog/post/list.html')
-    posts = Post.objects.filter(status="published")
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.objects.filter(status="published")
+    paginator = Paginator(object_list, 3)  # 3 кол-во постов на странице
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Если параметр страницы не является целым числом,
+        # мы извлекаем первую страницу результатов
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если этот параметр является числом, превышающим последнюю страницу
+        # результатов, мы извлекаем последнюю страницу.
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts})
 
 
 # Подробнее о нашем посте
